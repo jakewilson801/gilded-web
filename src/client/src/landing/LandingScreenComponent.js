fe37be00a1e83bb4498bbef8981dc9676a756e35
Jacob Wilson <jacob.grant.wilson.801@gmail.com>
2017-10-05 22:49:56 -0600

    Patched no results

--- src/client/src/landing/LandingScreenComponent.js
+++ src/client/src/landing/LandingScreenComponent.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import OccupationsComponent from '../occupations/OccupationsComponent.js';
 import URLUtils from '../util/URLUtils'
+import '../occupations/occupations.css'
+import {Link} from 'react-router-dom';
+
 class LandingScreenComponent extends Component {
-  state = {feed: []};
+  state = {feed: null};
 
   componentDidMount() {
     let t = URLUtils.getParameterByName('tuition');
@@ -20,12 +23,17 @@ class LandingScreenComponent extends Component {
   }
 
   render() {
-    if (this.state.feed.length > 0) {
-      return <div>
-        {this.state.feed.map( (row, index) => <OccupationsComponent key={index} fieldId={row.id} fieldTitle={row.title} occupations={row.occupations} />)}
-      </div>
-    } else {
+    if (!this.state.feed) {
       return <div>Loading...</div>
+    } else {
+      if(this.state.feed.length > 0) {
+        return <div>
+          {this.state.feed.map((row, index) => <OccupationsComponent key={index} fieldId={row.id} fieldTitle={row.title}
+                                                                     occupations={row.occupations}/>)}
+        </div>
+      } else {
+        return <div style={{margin: '10px'}}>No results for given salary, time and tuition <Link to={"/"}>Try again?</Link></div>
+      }
     }
   }
 }
