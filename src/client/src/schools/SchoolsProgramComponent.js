704478c93973c7bd2b2632c8d3389f685ed1db56
jakewilson801 <jacob.grant.wilson.801@gmail.com>
2017-12-15 16:22:53 -0700

    update labels for programs

--- src/client/src/schools/SchoolsProgramComponent.js
+++ src/client/src/schools/SchoolsProgramComponent.js
@@ -33,8 +33,8 @@ class SchoolsProgramComponent extends Component {
           <Typography type={"display1"}>{p.title}</Typography>
           <Typography>Cost: ${parseInt(p.cost_in_state, 10)}</Typography>
           <Typography>Length: {p.length_months} Months</Typography>
-          <Typography>{p.flexible_schedule ? "Flexible/Competency Based" : "Semester Based"}</Typography>
-          <Typography>Credential {p.credential}</Typography>
+          <Typography>Schedule: {p.flexible_schedule ? "Flexible/Competency Based" : "Semester Based"}</Typography>
+          <Typography>Credential: {p.credential}</Typography>
         </div>);
     } else {
       return <CircularProgress/>;
